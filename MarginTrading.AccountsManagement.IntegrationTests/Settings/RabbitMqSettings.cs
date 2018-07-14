7bbf7dd35422b2197c48f84ae9f547cc1d1b7c4e
Alexey Andreev <alexey.andreev@lykke.com>
2018-07-14 16:28:06 +0300

    MTC-198: remove not used rabbit settings

--- MarginTrading.AccountsManagement.IntegrationTests/Settings/RabbitMqSettings.cs
+++ MarginTrading.AccountsManagement.IntegrationTests/Settings/RabbitMqSettings.cs
@@ -5,7 +5,5 @@ namespace MarginTrading.AccountsManagement.IntegrationTests.Settings
     [UsedImplicitly]
     public class RabbitMqSettings
     {
-        public RabbitConnectionSettings AccountChangedExchange { get; set; }
-        public RabbitConnectionSettings AccountHistoryExchange { get; set; }
     }
 }
